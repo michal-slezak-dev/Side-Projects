7082e52058692c30fa9f980f9cae5fc922824884
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2024-03-05 19:53:47 +0100

    done-mobile-design

--- faq-accordion-main/js/app.js
+++ faq-accordion-main/js/app.js
@@ -1,22 +1,33 @@
-const btn = document.querySelectorAll('.question span');
+const btn = document.querySelectorAll('.question'); // or .question span
 const activeState = {}; // true - class=.active false - class!=.active
 
-for(let i = 0; i < btn.length; i++){
-    btn[i].addEventListener('click', function(){
-        const minsIcon = document.querySelector('.question' + (i + 1) + ' h2 span img');
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function () {
+        const icon = document.querySelector('.question' + (i + 1) + ' h2 span img');
         const answer = document.getElementById('a-' + (i + 1));
 
         answer.classList.toggle('active');
-        minsIcon.setAttribute('src', './assets/images/icon-minus.svg');
-        // fetch all visible answers using for loop
+
+
         const isActive = answer.classList.contains('active');
         activeState['a-' + (i + 1)] = isActive;
         // console.log(activeState);
 
-        for(let j = 0; j < btn.length; j++){
-            if(j != i){
+
+        // change button image
+        if (isActive) {
+            icon.setAttribute('src', './assets/images/icon-minus.svg')
+        } else {
+            icon.setAttribute('src', './assets/images/icon-plus.svg')
+        }
+
+
+        // reset so that only 1 answer is displayed at the time
+        for (let j = 0; j < btn.length; j++) {
+            if (j != i) {
                 const plusIcon = document.querySelector('.question' + (j + 1) + ' h2 span img');
                 const otherActiveAnswer = document.getElementById('a-' + (j + 1));
+
                 otherActiveAnswer.classList.remove('active');
                 plusIcon.setAttribute('src', './assets/images/icon-plus.svg');
                 activeState['a-' + (j + 1)] = false;
@@ -26,7 +37,7 @@ for(let i = 0; i < btn.length; i++){
 
 
 
+
+
     });
 }
-
-
