1f39c245e7dcc1ff7c1c5dfb5e72e503e28bd898
Michał Ślęzak <realslezak.michal@gmail.com>
2024-03-05 18:43:34 +0100

    update

--- faq-accordion-main/js/app.js
+++ faq-accordion-main/js/app.js
@@ -1,10 +1,32 @@
-const clickedBtns = document.querySelectorAll('span');
+const btn = document.querySelectorAll('.question span');
+const activeState = {}; // true - class=.active false - class!=.active
 
-clickedBtns.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-        const parent = this.closest('.question')
+for(let i = 0; i < btn.length; i++){
+    btn[i].addEventListener('click', function(){
+        const minsIcon = document.querySelector('.question' + (i + 1) + ' h2 span img');
+        const answer = document.getElementById('a-' + (i + 1));
 
-        parent.classList.toggle('active')
+        answer.classList.toggle('active');
+        minsIcon.setAttribute('src', './assets/images/icon-minus.svg');
+        // fetch all visible answers using for loop
+        const isActive = answer.classList.contains('active');
+        activeState['a-' + (i + 1)] = isActive;
+        // console.log(activeState);
 
-    })
-})
+        for(let j = 0; j < btn.length; j++){
+            if(j != i){
+                const plusIcon = document.querySelector('.question' + (j + 1) + ' h2 span img');
+                const otherActiveAnswer = document.getElementById('a-' + (j + 1));
+                otherActiveAnswer.classList.remove('active');
+                plusIcon.setAttribute('src', './assets/images/icon-plus.svg');
+                activeState['a-' + (j + 1)] = false;
+            }
+        }
+
+
+
+
+    });
+}
+
+
